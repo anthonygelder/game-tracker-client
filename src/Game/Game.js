ba9fde6569fcfebf6733ec0018404f84ad70b3b6
Anthony Gelder <anthony.gelder@gmail.com>
2020-04-28 22:37:01 -0700

    update status working, need to handle redirect

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,16 +1,69 @@
 import React, {Component} from 'react'
 import './Game.css'
-// const { API_ENDPOINT } = require('../config')
+const { API_ENDPOINT } = require('../config')
 
 class Game extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+      status: '',
+      options:``
     }
   }
 
+  updateGame() {
+    const editedStatus = { status: this.state.status }
+    fetch(`${API_ENDPOINT}/games/${this.props.game.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(editedStatus),
+      headers: {
+          'content-type': 'application/json'
+      },
+  })
+  .then(res => {
+      if (!res.ok) {
+          return res.json().then(error => {
+              throw error
+          })
+          }
+      })
+      .then(() => {
+          // this.props.routeProps.history.push('/entries')
+          console.log('heyo')
+      })
+      .catch(error => {
+          console.error(error)
+  })
+  }
+
+  changeStatus() {
+    this.setState({
+      status: this.props.game.status,
+      options: this.displaySelect()
+    })
+  }
+
+  onStatusChanged(status){
+    this.setState({status: status})
+  }
+
+  displaySelect() {
+    return (
+      <>
+        <select id="status" onChange={e => this.onStatusChanged(e.target.value)} >
+          <option value="Backlog">Backlog</option>
+          <option value="Just Started">Just started</option>
+          <option value="In Progress">In progress</option>
+          <option value="Almost Done">Almost done</option>
+          <option value="Complete">Complete</option>
+        </select>
+        <button onClick={() => {this.updateGame()}}>Update</button>
+      </>
+    )
+  }
+
   render() {
+    console.log(this.state)
     const { game } = this.props
     if(this.props.saved) {
       
@@ -21,7 +74,8 @@ class Game extends Component {
                   <img src={game.image} alt={'game'} className='cardImg' />
               </div>
               <h6><b>{game.game}</b></h6>
-              <h6><b>{game.status}</b></h6>
+              <h6><b onClick={() => {this.changeStatus()}}>{game.status}</b></h6>
+              {this.state.options}
               <p>{game.year}</p>
               <button onClick={() => {this.props.deleteGame(game.id)}}>
                 Delete
