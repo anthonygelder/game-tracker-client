4970040891f4004bc491d234fff9844730244ab6
Anthony Gelder <anthony.gelder@gmail.com>
2020-05-23 22:48:55 -0700

    logout button rendering when auth token is stored.

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
+import TokenService from '../services/token-service'
 import { Link } from 'react-router-dom'
 import './Header.css'
 
-function Header() {
-    return (
-        <header> 
-            <Link to='/' style={{ textDecoration: 'none' }}>
-                <h1>Game Tracker</h1>
-            </Link>
-            <div className='buttons'>
-                <Link to='/login'>
-                    <button>Login</button>
-                </Link>
-                <Link to='/register'>
-                    <button>Register</button>
+class Header extends Component {
+    handleLogoutClick = () => {
+        TokenService.clearAuthToken()
+    }
+
+    renderLogoutLink() {
+        return (
+            <div className='Header__logged-in'>
+                <Link onClick={this.handleLogoutClick} to='/'>
+                    <button>
+                        Logout
+                    </button>
                 </Link>
             </div>
-        </header>
-    )
+        )
+    }
+
+    renderLoginLink() {
+        return (
+            <div className='Header__not-logged-in buttons'>
+                <Link to='/register'>
+                    <button>
+                        Register
+                    </button>
+                </Link>
+                {/* <Hyph /> */}
+                <Link to='/login'>
+                    <button>
+                        Login
+                    </button>
+                </Link>
+            </div>
+        )
+    }
+    render() {
+        return (
+            <header> 
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                    <h1>Game Tracker</h1>
+                </Link>
+                <div>
+                    {TokenService.hasAuthToken()
+                    ? this.renderLogoutLink()
+                    : this.renderLoginLink()}
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Header;
